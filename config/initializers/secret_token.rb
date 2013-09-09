0abb8fb9dcc55be56296387d9fea9f4b9ac5acd9
PaulaEs <paulaproxy@gmail.com>
2013-09-10 01:20:48 +0200

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blogin20::Application.config.secret_key_base = '555f17bb66cb8b95b1378663d48e73beedc085f22be83087a80086fb2324efb1875330c99844dfb0f063cbb4cfa107d493a9142a8de82203dbd9a03c1ed9eaf4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Blogin20::Application.config.session_store :cookie_store, key: '_blogin20_session'

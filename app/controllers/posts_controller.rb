9a55cf7c0624b78945b1f636bbc86dadc603c191
PaulaEs <paulaproxy@gmail.com>
2013-09-10 01:59:46 +0200

    Index, new, edit.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,28 @@
 class PostsController < ApplicationController
+	respond_to :html
+
+	def index
+		@posts = Post.order("created_at desc")
+		respond_with @posts
+	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+		Post.create(post_params)
+		redirect_to posts_path
+
+	end	
+
+	def edit
+		@post = Post.find_by_id(params[:id])
+	end
+
+	private 
+		def post_params
+			params.require(:post).permit(:title, :body)
+		end
+
 end

17109fb37c6666c9611d0a5d692fd1af04c1f90c
PaulaEs <paulaproxy@gmail.com>
2013-09-17 16:55:15 +0200

    Spec for writing posts in Markdown and checking if correctly rendered in HTML.

--- spec/services/markdown_service_spec.rb
+++ spec/services/markdown_service_spec.rb
@@ -10,14 +10,14 @@ describe MarkdownService do
 		let(:markdown_engine) { double('Markdown') }
 
 		before do
-			# Stub out the redcarpet markdown engine
+			# Stub out the redcarpet markdown_engine
       # In our test we can assume it works properly
       # since it's a well tested library.
       Redcarpet::Markdown.stub(:new) { markdown_engine }
     end
 
     it 'should delegate to the markdown engine' do
-    	# Set up the epxectation of what our code should accomplish
+    	# Set up the expectation of what our code should accomplish
     	markdown_engine.should_receive(:render).with(content)
     	MarkdownService.new.render(content)
     end

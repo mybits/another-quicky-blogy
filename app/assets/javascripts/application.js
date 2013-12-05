cfacd045bfa876f718e602d89830f6909eea686c
PaulaEs <paulaproxy@gmail.com>
2013-12-06 00:15:35 +0100

    Starting to implement rating feature using raty.js library.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require foundation
+//= require jquery.raty.min.js
 //= require_tree .
 
 $(document).foundation();
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130916152118) do
+ActiveRecord::Schema.define(version: 20131205225812) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -60,5 +60,16 @@ ActiveRecord::Schema.define(version: 20130916152118) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "ratings", force: true do |t|
+    t.integer  "comment_id"
+    t.integer  "user_id"
+    t.integer  "score",      default: 0
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "ratings", ["comment_id"], name: "index_ratings_on_comment_id"
+  add_index "ratings", ["user_id"], name: "index_ratings_on_user_id"
 
 end
